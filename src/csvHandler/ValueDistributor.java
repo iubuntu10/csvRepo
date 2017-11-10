339699a342b2ff3289aa2d8fecf858818912c563
garyg <garyg@crystalintl.com>
2017-11-10 14:23:25 -0500

    ValueDistributor.java is done

--- src/csvHandler/ValueDistributor.java
+++ src/csvHandler/ValueDistributor.java
@@ -4,6 +4,7 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.text.NumberFormat;
 import java.text.ParseException;
+import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Locale;
@@ -28,13 +29,16 @@ public class ValueDistributor {
 		String[] sLine;
 		String cHeader = "";
 		boolean isNewMat = false;
-		double total = 0;
+		double qty = 0;
+		double totalQty = 0;
+		double totalValue = 0;
+		double eachValue = 0;
 		int counter = 0;
-		Map<Double, Double> map = null;
+		ArrayList<Double> arr = new ArrayList<Double>();
 		
 		try {
 
-			NumberFormat format = NumberFormat.getInstance(Locale.FRANCE);
+			NumberFormat format = NumberFormat.getInstance();
 		    Number number;
 			
 			source = new CSVReader(new FileReader("C://Users/Garyg/Desktop/MasterData/Parts Location Qty with Trace ID_DONE.csv"), '|', '"', 1);
@@ -42,44 +46,60 @@ public class ValueDistributor {
 			
 //			List<String[]> tLines = target.readAll();
 			
-			while ((sLine = source.readNext()) != null) {
-				if (!(cHeader.contentEquals(sLine[0]))){
-					
-					if (total != 0){
-						System.out.println(cHeader);
+			 do {
+				 //read each line
+				sLine = source.readNext(); 
+
+				//if it's last mat
+				if (sLine == null){
+//					System.out.println(cHeader);
+					for (Double batchQty: arr){
+						totalQty += batchQty;
 					}
-					
+//					System.out.println("value per unit = "+totalValue/totalQty);
+					for (Double batchQty: arr){
+						eachValue = batchQty*(totalValue/totalQty);
+						System.out.println(String.format("%,.2f", eachValue));
+					}
+				//if it's a new mat
+				}else if (!(cHeader.contentEquals(sLine[0]))){
 					isNewMat = true;
 					
-					if (map!=null){
-						for (Double key: map.keySet()){
-							total += key;
+					number = format.parse(sLine[5]);
+					qty = number.doubleValue();
+					
+					//print the list, and init the array
+					if (!arr.isEmpty()){
+//						System.out.println(cHeader);
+						for (Double batchQty: arr){
+							totalQty += batchQty;
 						}
+//						System.out.println("value per unit = "+totalValue/totalQty);
+						for (Double batchQty: arr){
+							eachValue = batchQty*(totalValue/totalQty);						
+							System.out.println(String.format("%,.2f", eachValue));
+						}
+						totalQty = 0;
+						arr = new ArrayList<Double>();
 					}
 					
-					
-					
-					map = new LinkedHashMap<Double,Double>();
-					
-					
-					
+					cHeader = sLine[0];					
+					totalValue = format.parse(sLine[8]).doubleValue();
+				//or it'd be same mat
 				} else {
 					isNewMat = false;
+					number = format.parse(sLine[5]);
+					qty = number.doubleValue();
 				}
 				
-				if(!isNewMat){
-					
-				} else {
-					cHeader = sLine[0];
-				}
+				arr.add(qty);
 				
-				
-			}
+			} while (sLine != null);
 			
 			source.close();
 //			target.close();
 			
-		}catch (IOException e) {
+		}catch (IOException | ParseException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 
